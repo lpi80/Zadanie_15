47764b7ba4de312b78c7861fe602968016d2bfc3
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-01-15 22:09:55 +0100

    Add new methods

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, system, country) {
 	this.brand = brand;
 	this.price = price;
-	this.color = color;
+    this.color = color;
+    
+    this.system = system;
+    this.country = country;
 }
 
 Phone.prototype.printInfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ",<br>");
+    console.log("Operation system " + this.system + ", produce in " + this.country + ".");
 }
 
-const iPhone6S = new Phone("Apple", 2250, "silver");
-const onePlusOne = new Phone("One", 500, "gold");
-const samsungGalaxyS6 = new Phone("Samsung", 2500, "white");
+const iPhone6S = new Phone("Apple", 2250, "silver", 'OS', 'USA');
+const onePlusOne = new Phone("One", 500, "gold", "Android", "China");
+const samsungGalaxyS6 = new Phone("Samsung", 2500, "white", "Android", "Korea");
